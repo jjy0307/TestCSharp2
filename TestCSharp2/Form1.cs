588de426287f9c9afc53bac821598aa47b11bbd2
jjy0307 <adz7546@gmail.com>
2025-04-28 12:26:07 +0900

    Mod if else

--- TestCSharp2/Form1.cs
+++ TestCSharp2/Form1.cs
@@ -39,28 +39,30 @@ namespace TestCSharp2
             int priceA3 = 1200000;
             float discount = 0.15f;
             byte stock = 8;
-            bool isAvailable = stock >= 1;
+            //bool isAvailable = stock >= 1;
+            bool isAvailable = (stock >= 1) ? true : false;
 
             double discounted = Math.Round(priceA3 * (1 - discount), 0);
 
-            if (isAvailable)
-            {
-                textBox1.Text += $"구매 가능: 할인 가격은 {discounted}원 입니다.";
-            }
-            else
-            {
-                textBox1.Text += "품절되었습니다.";
-            }
-
-            if (stock >= 5)
-            {
-                textBox2.Text += "여유 있음";
-            }
-            else
-            {
-                textBox2.Text += "소량 남음";
-            }
-
+            //if (isAvailable)
+            //{
+            //    textBox1.Text += $"구매 가능: 할인 가격은 {discounted}원 입니다.";
+            //}
+            //else
+            //{
+            //    textBox1.Text += "품절되었습니다.";
+            //}
+            string resultMessage = (stock > 0) ? $"구매 가능: 할인 가격은 {discounted}원 입니다." : "품절되었습니다.";
+            textBox1.Text += resultMessage;
+            //if (stock >= 5)
+            //{
+            //    textBox2.Text += "여유 있음";
+            //}
+            //else
+            //{
+            //    textBox2.Text += "소량 남음";
+            //}
+            textBox2.Text += (stock >= 5) ? "여유 있음" : "소량 남음";
             textBox3.Text += $"상품명: {productName}, 할인된 가격: {discounted}원, 재고: {stock}개";
         }
     }
